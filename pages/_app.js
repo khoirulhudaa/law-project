2e5bfa2f5bb2bb09c96ecac268a139710bfa7630
valclassdevelop <developervalclass@gmail.com>
2022-01-06 21:17:46 +0700

    landing page - Law

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap'
+import "../styles/global/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return( 
+    <>
+   <Component {...pageProps} />
+    </>
+   )
 }
 
 export default MyApp
